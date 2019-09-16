1c6190c59f3fe2b8a3c9582d23e7a7db3fc68ede
Johan van Soest <johan.vansoest@maastro.nl>
2019-09-16 09:36:23 +0200

    Fixed output description info

--- pyScripts/uploadData.py
+++ pyScripts/uploadData.py
@@ -22,25 +22,28 @@ transactionUrl = transactionRequest.headers["Location"]
 #query = "DROP GRAPH <%s>" % graphName
 query = "DROP ALL;"
 
-print("start dropping database: " + datetime.now())
+print("start dropping database: " + str(datetime.now()))
 dropRequest = requests.put(transactionUrl + "?action=UPDATE&update=%s" % query)
 commitRequest = requests.put(transactionUrl + "?action=COMMIT")
-print("done dropping database: " + datetime.now())
+print("done dropping database: " + str(datetime.now()))
 
 #####################
 # Load RDF store with new data
 #####################
 
-print("Read ttl file: " + datetime.now())
+num_lines = sum(1 for line in open('/output.ttl'))
+print("Number of triples: %s" % num_lines)
+
+print("Read ttl file: " + str(datetime.now()))
 turtle = ""
 with open('/output.ttl', 'r') as myfile:
     turtle=myfile.read()
 
-print("Start uploading triples: " + datetime.now())
+print("Start uploading triples: " + str(datetime.now()))
 loadRequest = requests.post((outputEndpoint + "/statements?context=%3C" + graphName + "%3E"),
     data=turtle, 
     headers={
         "Content-Type": "application/x-turtle"
     }
 )
-print("Done uploading triples: " + datetime.now())
+print("Done uploading triples: " + str(datetime.now()))
