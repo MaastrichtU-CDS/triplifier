c32ba62c59a4624c3cb340b515c0242c4ec23df2
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2025-06-11 10:48:55 +0200

    Removed all legacy (java) code

--- containerTest/setupGraph.bat
+++
@@ -1,1 +0,0 @@
-docker run --rm -d --name graphdb -p 7200:7200 jvsoest/graphdb-free:fairstation
--- containerTest/testGraph.bat
+++
@@ -1,4 +0,0 @@
-docker run --rm --link postgresdb:dbhost ^
-    --link graphdb:graphdb ^
-    -e SLEEPTIME=60 ^
-    jvsoest/test
